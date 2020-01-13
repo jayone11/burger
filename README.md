190456e49512243d04d845161a020cddd55a9bbb
Jeff Barroso <jb1@Jeffs-MBP.lan1>
2020-01-13 11:20:33 -0800

    develop branch push

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # burger
+# Eat-Da-Burger
+
+A simple CRUD application.
+![](screenshot-1.png)
+
+Live App: 
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Big Mac');
+INSERT INTO burgers (name) VALUES ('Whopper');
+INSERT INTO burgers (name, devoured) VALUES ('Double-Double', true);
+INSERT INTO burgers (name, devoured) VALUES ('Beyond burgers', true);
+INSERT INTO burgers (name, devoured) VALUES ('Tommy''s burgers', true);
+INSERT INTO burgers (name) VALUES ('Shake Shack burgers');
