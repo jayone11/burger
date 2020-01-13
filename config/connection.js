5d5b665b303c5ab3f7a748e105c82eb6cdd0b24c
Jeff Barroso <jb1@Jeffs-MBP.lan1>
2020-01-13 14:58:22 -0800

    updated mysql info

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "gojila1171",
+    user: "wqv2t4wrp7fm87ad",
+    password: "ntpjgqg89u1zpwdd",
     database: "burger_db"
   });
 }
